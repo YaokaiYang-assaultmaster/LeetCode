760413bd98d9fb07e1e569e89053df5bb66240ed
Yaokai Yang <yxy586@case.edu>
2017-06-12 22:59:19 -0700

    Create 106. Construct Binary Tree from Inorder and Postorder Traversal.java

--- LeetcodeQuestions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
+++ LeetcodeQuestions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -20,6 +20,8 @@ public class Solution {
      
      * The time complexity would be O(n/2 + n / 4 + n / 8 + ... + 1) = O(n) time on average considering the binary is approximately balance. Worst case O(n + n - 1 + n - 2 + ... + 1) = O(n^2) when the binary tree is like a linkedlist. 
      * See here for proof: https://math.stackexchange.com/questions/401937/how-is-nn-2n-4-1-equal-to-2n-1-using-the-formula-for-geometric-series. 
+     * A small optimization to the time would be to add a HashMap store each number and its corresponding index of inorder array. Thus we shortens the time of looking up root position of each level to O(1), thus make the total time to be O(n) stably. But the space would be O(n) in this case. 
+     
      * The space complexity would be O(1) without the recursion stack. And O(logn) on average with the recursion stack. O(n) worst case. These conditions are the same with time. 
      */
     public TreeNode buildTree(int[] inorder, int[] postorder) {
