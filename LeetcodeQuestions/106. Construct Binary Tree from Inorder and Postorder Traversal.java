c3cc0f2439fe871beb9b2f06b51132616917b3f0
Yaokai Yang <yxy586@case.edu>
2017-06-12 22:53:13 -0700

    Create 106. Construct Binary Tree from Inorder and Postorder Traversal.java

---
+++ LeetcodeQuestions/106. Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**(1) Recursive method.
+     * Since it's a binary tree question, recursion might be the apporpriate solution. 
+     * To constructr a binary tree is to find out the root of each level. 
+     * Thus for this question, the basic idea should be to find out the root of each level. 
+     * For the first level, the root is simply the last node in postorder traversal. 
+     * And based on this point, we can devide the inorder array into 2 parts. 
+     * The left part of the array is the left subtree of root, and the right part of the array is the right subtree of the root.
+     * Thus based on the length of left and right subtree, we could get the left sub-root and right-root again. 
+     * Do it  recursively and we got the whole tree. 
+     
+     * The time complexity would be O(n/2 + n / 4 + n / 8 + ... + 1) = O(n) time on average considering the binary is approximately balance. Worst case O(n + n - 1 + n - 2 + ... + 1) = O(n^2) when the binary tree is like a linkedlist. 
+     * See here for proof: https://math.stackexchange.com/questions/401937/how-is-nn-2n-4-1-equal-to-2n-1-using-the-formula-for-geometric-series. 
+     * The space complexity would be O(1) without the recursion stack. And O(logn) on average with the recursion stack. O(n) worst case. These conditions are the same with time. 
+     */
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if (inorder == null || postorder == null) {
+            return null;
+        }
+        if (inorder.length == 0 || postorder.length == 0) {
+            return null;
+        }
+        if (inorder.length != postorder.length) {
+            return null;
+        }
+        
+        return buildTree(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
+    }
+    
+    public TreeNode buildTree(int[] inorder, int[] postorder, int startInorder, int endInorder, int startPostorder, int endPostorder) {
+        // Notice that the length of inorder and postorder array in this function should always be the same.
+        // Since they only contain different traversal sequence of the same binary tree.  
+    
+        // If the length of inorder array and postorder array is 0, return null since it indicates there is no node in current branch. 
+        if (endInorder < startInorder) {
+            return null;
+        }
+        // If the length of inorder array and postorder array is 1, return the node directly. 
+        if (endInorder == startInorder) {
+            return new TreeNode(inorder[startInorder]);
+        }
+        
+        int rootNum = postorder[endPostorder];
+        int rootPos = -1;
+        TreeNode root = new TreeNode(rootNum);
+        
+        for (int i = startInorder; i <= endInorder; i++) {
+            if (inorder[i] == rootNum) {
+                rootPos = i;
+                break;
+            }
+        }
+        
+        int leftLen = rootPos - startInorder;
+        
+        root.left = buildTree(inorder, postorder, startInorder, startInorder + leftLen - 1, startPostorder, startPostorder + leftLen - 1);
+        
+        root.right = buildTree(inorder, postorder, startInorder + leftLen + 1, endInorder, startPostorder + leftLen,  endPostorder - 1);
+        
+        return root;
+    }
+}
